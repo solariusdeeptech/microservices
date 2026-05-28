794943066e32206c29bc013862828ead1165f9d7
solariusdeeptech <solariustechno@gmail.com>
2026-05-28 18:36:14 +0200

    fix: julia-geostat/install_packages.jl - no Project.toml copy, fresh Pkg.add

--- julia-geostat/install_packages.jl
+++ julia-geostat/install_packages.jl
@@ -1,16 +1,5 @@
-# Install packages - run with: julia install_packages.jl
 using Pkg
-Pkg.activate(".")
-# Remove any stale Manifest
-rm("Manifest.toml", force=true)
-# Only add packages that are NOT already merged into GeoStats
-Pkg.add([
-    "HTTP",
-    "JSON3",
-    "GeoStats",
-    "Meshes",
-    "GeoTables",
-    "Distributions"
-])
+Pkg.activate("/app")
+Pkg.add(["HTTP", "JSON3", "GeoStats", "Meshes", "GeoTables", "Distributions"])
 Pkg.precompile()
-@info "All packages installed and precompiled"
+println("ALL PACKAGES INSTALLED OK")
